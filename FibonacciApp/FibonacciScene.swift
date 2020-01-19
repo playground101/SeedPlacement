6b82b3934785f3507c08907dd3c13d030e78effd
kisamekaze <anish6973@gmail.com>
2020-01-19 10:29:56 -0600

    mostly done

--- FibonacciApp/FibonacciScene.swift
+++ FibonacciApp/FibonacciScene.swift
@@ -7,3 +7,96 @@
 //
 
 import Foundation
+import SpriteKit
+
+class FibonacciScene: SKScene {
+    
+    var seeds = 10
+    
+    override func didMove(to view: SKView) {
+        //        oldDegrees = degrees
+        //        for i in 1...seeds {
+        //            Timer.scheduledTimer(withTimeInterval: 0.4, repeats: false) { (timer) in
+        //                self.spawnCircle()
+        //            }
+        //        }
+    }
+    
+    var positionX:Double = 0.0
+    var addX: Double = 0
+    var positionY:Double = 0.0
+    var addY: Double = 0
+    var ratio: Double = 0.48
+    var radius = 30.0
+    var circumference: Double = 0
+    var oldDegrees: Double = 0.0
+    var degrees = 0.0
+    var totalDegrees = 0.0
+    var distance: Double = 0
+    var tracker: Int = 1
+    var factor = 0.0
+    
+    func getPosition(degrees: Double) -> CGPoint {
+        let y = radius * cos(Double(degrees))
+        let x = radius * sin(Double(degrees))
+        return CGPoint(x: x, y: y)
+    }
+    func start(seedNumber: Int, ratio: Double) {
+        reset()
+        self.ratio = ratio
+        seeds = seedNumber
+        oldDegrees = ratio * 360
+        for i in 1...seeds {
+            Timer.scheduledTimer(withTimeInterval: 0.01, repeats: false) { (timer) in
+                self.spawnCircle()
+            }
+        }
+    }
+    func reset() {
+        removeAllChildren()
+        factor = 0.0
+        positionX = 0.0
+        addX = 0
+        positionY = 0.0
+        addY = 0
+        ratio = 0.48
+        radius = 30.0
+        circumference = 0
+        oldDegrees = 0.0
+        degrees = 0.0
+        totalDegrees = 0.0
+        distance = 0
+        tracker = 0
+        seeds = 10
+        
+    }
+    func spawnCircle() {
+        degrees += oldDegrees
+        let radians = (CGFloat(degrees) * CGFloat(2) * CGFloat.pi)/360
+        //print(radians)
+        let node = SKShapeNode(circleOfRadius: 5)
+        //print("degrees:\(oldDegrees)")
+        totalDegrees += oldDegrees
+        //print("totalDegrees:\(totalDegrees)")
+        node.alpha = 0
+        node.fillColor = UIColor.red
+        node.position = CGPoint(x: sin(radians) * CGFloat(radius),
+                                y: cos(radians) * CGFloat(radius))
+        if totalDegrees > 360.0 {
+            totalDegrees = 0
+            print("\(tracker): REMAINDER IS \(totalDegrees.truncatingRemainder(dividingBy: 360.0))")
+            factor += 2
+            print("factor: \(factor), radius: \(radius)")
+            node.position = CGPoint(x: (sin(radians) * CGFloat(radius + factor)), y: (cos(radians) * CGFloat(radius + factor)))
+        }
+        let action = SKAction.fadeAlpha(to: 1, duration: 0.01)
+        // print("\(tracker): \(radians) radians")
+        print("\(tracker)-  x value: sin(\(radians)) = \(sin(radians))")
+        tracker += 1
+        addChild(node)
+        node.run(action)
+        print(node.position)
+        //        degrees += oldDegrees
+    }
+}
+
--- FibonacciApp/ViewController.swift
+++ FibonacciApp/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
+    var seeds: Int?
+    var ratio: Double?
+    @IBOutlet weak var fibonacciSKView: SKView!
+    @IBOutlet weak var seedNumber: UILabel!
+    @IBAction func increaseRatio(_ sender: UIStepper) {
+        ratio? += Double(sender.value)
+        //print("Description: \(ratio?.description)")
+        ratioTextField.text = ratio?.description
+        if let ratio = ratio, let seeds = seeds {
+            fib.reset()
+            fib.start(seedNumber: seeds, ratio: ratio)
+            print("seeds: \(seeds)")
+        }
+    }
+    @IBOutlet weak var ratioIncrement: UIStepper!
+    var fib = FibonacciScene()
+    @IBOutlet weak var ratioTextField: UITextField!
+    @IBOutlet weak var incrementTextField: UILabel!
+    @IBOutlet weak var seedTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ratioTextField.delegate = self
+        seedTextField.delegate = self
+        if let scene = SKScene(fileNamed: "FibonacciScene") {
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFit
+            fib = scene as! FibonacciScene
+            // Present the scene
+            fibonacciSKView.presentScene(scene)
+            
+        }
     }
-
-
 }
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(textField)
+        self.view.endEditing(true)
+        seeds = Int(seedTextField.text ?? "10")
+        ratio = Double(ratioTextField.text ?? "0.1")
+        let increment = Double(incrementTextField.text ?? "0.001")
+        ratioIncrement.stepValue = increment ?? 0.001
+        if let seeds = seeds, let ratio = ratio {
+            fib.start(seedNumber: seeds, ratio: ratio)
+        }
+        return true
+    }
+}
+
 
